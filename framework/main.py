1774baa9d878f0fb8103ac27a7d28e38699acfda
wwendler <willis.wendler@gmail.com>
2011-04-03 01:09:20 -0400

    updated framework twisted stuff, still figuring out how to make stuff work using windows

--- framework/main.py
+++ framework/main.py
@@ -3,91 +3,95 @@
 
 import sys
 
+# defer.Deferred()
 from twisted.internet import defer
-from twisted.internet.protocol import Protocol, ClientFactory
+from twisted.internet.protocol import Protocol, Factory, ClientFactory
 from twisted.protocols.basic import NetstringReceiver
-#from twisted.internet.protocol import DatagramProtocol
 
-class GameProtocol(DatagramProtocol):
-    strings = ["ohai", "halloes", "hello and goodbye"]
-    def __init__(self, reactor, port):
-        self.reactor = reactor
-        self.port = int(port)
-    def addIP(self, ip):
-        print 'adding %s %s' % (ip, self.port)
-        self.transport.connect(ip, self.port)
-    def doStuff(self, _):
-        self.sendDatagram()
-    #def startProtocol(self):
-    #    pass
-        #self.transport.connect(self.ip, self.port)
-        #self.sendDatagram()
-    def sendDatagram(self):
-        if len(self.strings):
-            datagram = self.strings.pop(0)
-            print "sending datagram... " + repr(datagram)
-            self.transport.write(datagram)
-    def datagramReceived(self, datagram, (host, port)):
-        print ("Datagram received from %s %s: %s"
-                % (host, port, repr(datagram)))
-        self.sendDatagram()
-
-class ServerProtocol(DatagramProtocol):
-    def __init__(self, self.port)...
-    def datagramReceived(self, datagram, address):
-        new_msg = datagram + '~'
-        self.transport.write(new_msg, address)
+class GameProtocol(NetstringReceiver):
+    def stringReceived(self, line):
+        if line == "quit":
+            print "received disconnect request?"
+            self.transport.loseConnection()
+        else:
+            print "message received: %s" % line
 
 class GameFactory(ClientFactory):
     protocol = GameProtocol
-    def __init__(self, deferred):
-        self.deferred = deferred
+    def __init__(self):
         print "game factory initialized"
-    def endCommunication(self):
-        if self.deferred is not None:
-            d, self.deferred = self.deferred, None
-            d.callback(0)
+    def startFactory(self):
+        print "starting client factory"
+    def stopFactory(self):
+        print "halting client factory"
+    def startedConnecting(self):
+        print "connecting..."
+    def clientConnectionFailed(self):
+        print "failed to connect!"
+    def clientConnectionLost(self):
+        print "lost a connection"
 
-class Game():
-    def __init__(self, name):
-        self.factory = GameFactory(name)
+class Game(object):
+    def __init__(self):
+        self.factory = GameFactory()
         print "game initialized"
     def connect(self, host, port):
         from twisted.internet import reactor
         reactor.connectTCP(host, port, self.factory)
         reactor.run()
 
-def main():
-    from twisted.internet import reactor
-    if sys.argv[1] == 'server':
-    	protocol = ServerProtocol()
-    	reactor.listenUDP(int(sys.argv[2]), protocol)
-    else:
-    	#d = defer.Deferred()
-    	protocol = GameProtocol(reactor, int(sys.argv[2]))
-    	d = reactor.resolve(sys.argv[1])
-    	d.addCallback(protocol.addIP)
-    	d.addCallback(protocol.doStuff)
-        t = reactor.listenUDP(0, protocol)
-    reactor.run()
+class GameServerProtocol(NetstringReceiver):
+    def stringReceived(self, line):
+        print "message received: %s" % line
+        self.num_lines_received += 1
+        if (self.num_lines_received > 5):
+            self.sendString("quit")
+    def connectionMade(self):
+        print "connection made."
+        self.num_lines_received = 0
+        self.sendString("y hello thar\n")
+    def connectionLost(self):
+        print "connection lost."
 
-def main2():
-    if len(sys.argv) == 4:
-    	print 'I am a client'
-        name = sys.argv[1]
-        ip = sys.argv[2]
-        port = int(sys.argv[3])
-        lost = Game(name)
-        lost.connect(ip, port)
-    elif len(sys.argv) == 2:
-        print 'I am a server'
-        bork = Server()
-        port = int(sys.argv[1])
-        # ???
+class GameServerFactory(Factory):
+    protocol = GameServerProtocol
+    def __init__(self):
+        print "game server factory initialized"
+    def startFactory(self):
+        print "server factory started"
+    def stopFactory(self):
+        print "server factory stopped"
+
+class GameServer(object):
+    def __init__(self):
+        self.factory = GameServerFactory()
+        print "game server initialized"
+    def listen(self, port):
+        from twisted.internet import reactor
+        reactor.listenTCP(port, self.factory)
+        reactor.run()
+
+def main():
+    print "starting up..."
+    if len(sys.argv) < 2:
+        print "incorrect usage."
+        print "for a server: 'server [port]'"
+        print "for a client: '[ip] [port]'"
+        return
+    if sys.argv[1] == 'server':
+        print "i am a server"
+        server = Server()
+        port = int(sys.argv[2])
+        protocol = ServerProtocol()
+        print "starting server on port %i" % port
+        server.listen(port)
     else:
-    	print "wrong number of args"
-        print "client: [name] [ip] [port]"
-        print "server: [port]"
+        print "i am a client"
+        game = Game()
+        ip = sys.argv[1]
+        port = int(sys.artv[2])
+        print "connecting to %s port %i" % (ip, port)
+        game.connect(ip, port)
 
 if __name__ == "__main__":
     main()
